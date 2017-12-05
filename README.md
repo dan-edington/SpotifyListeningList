f3cb3c09612799c5c04fa2d59af7ccbe91e7e0d6
Dan Edington <dan2505@hotmail.com>
2017-12-05 12:01:18 +0000

    Updated readme

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 Simple React app which consumes the Spotify API to search for albums and add them to a todo list.
 Currently, todos are saved in browser localstorage. Will probably update to database storage in the future.
+
+Built using create-react-app so the usual commands apply:
+
+Start a dev build:
+
+```
+npm start
+```
+
+Start a production build:
+
+```
+npm build
+```
+
+Prior to running, make sure src/config/SpotifyCredentials.js exists and contains the correct details.
--- src/config/SpotifyCredentials.example.js
+++ src/config/SpotifyCredentials.example.js
@@ -1,13 +1,13 @@
 const SpotifyCredentials = {
   production: {
-    clientID: 'cee894f275734341a6dd69a408adbc70',
-    clientSecret: '51b9f52e3eb34a74bc674fcbd1dfe70b',
-    callbackURL: 'http://www.danedington.com/spotifyToListenTo/',
+    clientID: '',
+    clientSecret: '',
+    callbackURL: '',
   },
   development: {
-    clientID: 'cee894f275734341a6dd69a408adbc70',
-    clientSecret: '51b9f52e3eb34a74bc674fcbd1dfe70b',
-    callbackURL: 'http://localhost:3000/',
+    clientID: '',
+    clientSecret: '',
+    callbackURL: '',
   },
 };
 
